c3accae5d6ad4e69bdfcbe51cd0293594492eead
rutiksimform <rutik.p@simformsolutions.com>
2022-02-24 18:55:06 +0530

    Folder Delete

--- JavaScript/TernaryOperator..js
+++
@@ -1,9 +0,0 @@
-// ternary operator takes 3 operands
-// varname = (condition) ? value1:value2
-
-var age = 21;
-// (age>18) ? "age is more than 18":"age is less than 18"
-console.log((age>18) ? "age is more than 18":"age is less than 18")
-
-var marks = 50;
-console.log((marks>55) ? "Pass" : "Fail");
